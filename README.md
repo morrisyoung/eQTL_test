42c6e5c2c6a3b0009b1ef929e18586b1bb194308
morrisyoung <sdmorrisys@gmail.com>
2015-09-14 17:11:37 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # eQTL_test
+
+
+1. checking the predictive precision;
+
+2. visualizing some results
+
+
---
+++ test.py
@@ -0,0 +1,8 @@
+## this is used to test the parameters we learned from the main program
+## this will use the testing portion of all the expression data
+
+
+
+if __name__ == '__main__':
+
+	##
